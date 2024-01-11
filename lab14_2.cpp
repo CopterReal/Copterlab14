da9a70159c5409c973efc1a60a7fb2586d4a9d89
Copter <copter101x18@gmail.com>
2024-01-11 22:42:53 +0700

    Completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,37 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double matrix[][N]){
+	for(int i=0; i<N; i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j=0; j<N; j++){
+			cin >> matrix[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+	for (int i = 0; i < N; ++i){
+        B[i][0] = B[i][N - 1] = B[0][i] = B[N - 1][i] = 0;
+	}
+	for (int i = 1; i < N - 1; ++i) {
+        for (int j = 1; j < N - 1; ++j) {
+            if (A[i][j] >= A[i - 1][j] && A[i][j] >= A[i + 1][j] &&
+                A[i][j] >= A[i][j - 1] && A[i][j] >= A[i][j + 1]) {
+                B[i][j] = true; 
+            } else {
+                B[i][j] = false; 
+            }
+        }
+    }
+}
+
+void showMatrix(const bool matrix[][N]){
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			cout << matrix[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
